d01d3a03654b78d549ab5a44ab7e15728eec0859
DornPorn <gungan722121@gmail.com>
2022-08-09 10:59:46 +0900

    Initial Commit

---
+++ SpacenewsReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpacenewsReader
+//
+//  Created by Stanislav Rassolenko on 8/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
